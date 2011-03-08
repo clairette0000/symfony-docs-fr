49687a2b4114f1f99ff9afe4b9575f36926819e8
Denis <denis.chartier@bonjour-tic.com>
2011-03-08 16:23:21 +0100

    added doc generator

---
+++ gendoc.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+reset
+echo "◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘"
+echo "◘◘◘ INITIALISATION ◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘"
+echo "◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘"
+echo ""
+echo " * * * * *  The generation will start in 5 secondes. | CTRL+Z for cancel"
+sleep 1
+echo "   * * * *  ---------------------------- 4 secondes. | CTRL+Z for cancel"
+sleep 1
+echo "     * * *  ---------------------------- 3 secondes. | CTRL+Z for cancel"
+sleep 1
+echo "       * *  ---------------------------- 2 secondes. | CTRL+Z for cancel"
+sleep 1
+echo "         *  ---------------------------- 1 seconde.  | CTRL+Z for cancel"
+sleep 1
+echo "                        GO !"
+echo ""
+echo "◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘"
+echo ""
+mkdir _static
+sleep 1
+mkdir _templates
+sleep 1
+mkdir _build
+sleep 1
+make html
+echo "Finished."
